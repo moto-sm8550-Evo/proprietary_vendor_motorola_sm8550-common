68dd3c77aaa558eb78d9771c917984b19cbbc565
Nick Reuter <nreuter85@gmail.com>
2026-04-15 23:31:33 -0500

    sm8550-common: Sort bluetooth blobs
    Change-Id: I9939d2e8b87d872bc311a8a21e4c5713878ebb8f

--- sm8550-common-vendor.mk
+++ sm8550-common-vendor.mk
@@ -802,7 +802,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.dsp@1.0 \
     vendor.qti.hardware.embmssl@1.0 \
     vendor.qti.hardware.embmssl@1.1 \
-    vendor.qti.hardware.fm@1.0_vendor \
+    vendor.qti.hardware.fm@1.0 \
     vendor.qti.hardware.mwqemadapter@1.0 \
     vendor.qti.hardware.pal@1.0-impl \
     vendor.qti.hardware.qccsyshal@1.0_vendor \
